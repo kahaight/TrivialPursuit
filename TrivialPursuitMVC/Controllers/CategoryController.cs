43e3ef415f407b3652ecda28b1c7cee28a5ea4c4
Konrad Haight <konradhaight@gmail.com>
2020-04-05 17:21:45 -0400

    hadn't in a while

--- TrivialPursuitMVC/Controllers/CategoryController.cs
+++ TrivialPursuitMVC/Controllers/CategoryController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrivialPursuit.Models.Category;
 
 namespace TrivialPursuitMVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class CategoryController : Controller
     {
         // GET: Category
         public ActionResult Index()
         {
+            var model = new CategoryListItem[0];
             return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CategoryCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
---
+++ TrivialPursuitMVC/Controllers/VersionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrivialPursuit.Models.Version;
+
+namespace TrivialPursuitMVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class VersionController : Controller
+    {
+        // GET: Version
+        public ActionResult Index()
+        {
+            var model = new VersionListItem[0];
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
