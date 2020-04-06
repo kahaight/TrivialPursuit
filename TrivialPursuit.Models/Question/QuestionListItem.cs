40730e06958f6e73522d6b57d9f5d36a003c2221
Konrad Haight <konradhaight@gmail.com>
2020-04-06 18:31:26 -0400

    question detail and list item partial views working

--- TrivialPursuit.Models/Question/QuestionListItem.cs
+++ TrivialPursuit.Models/Question/QuestionListItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace TrivialPursuitMVC.Models.Question
+namespace TrivialPursuit.Models.Question
 {
     public class QuestionListItem
     {
